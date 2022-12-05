5fdcc6ff93935efba93b5093992c076936dbc80a
official-paras-bhandari <parashbhandari85@gmail.com>
2022-12-05 09:22:14 +0545

    Day23

--- Day23/main.py
+++ Day23/main.py
@@ -7,8 +7,27 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+player = Player()
 
+screen.onkey(fun=player.move_up, key="Up")
+car_manager = CarManager()
+scoreboard = Scoreboard()
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    screen.update()
+    screen.update()  # refreshing every 0.1 second
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # Detect collision with cars
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    # Detect when player finish the line
+    if player.is_finish_the_line():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.score_increase()
+screen.exitonclick()
--- Day23/player.py
+++ Day23/player.py
@@ -1,7 +1,27 @@
+from turtle import Turtle, Screen
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(0, new_y)
+
+    def goto_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_finish_the_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
