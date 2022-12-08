b07369809aadf94418bdb93723814a69fc46a1a0
official-paras-bhandari <parashbhandari85@gmail.com>
2022-12-08 23:15:48 +0545

    Day24,organized

--- Day24/file_read_write/write_read.py
+++ Day24/file_read_write/write_read.py
@@ -10,11 +10,11 @@ with open("my_file.txt") as file:
     print(content)
 
 # Writing
-with open("my_file.txt",mode="w") as file:
+with open("my_file.txt", mode="w") as file:
     file.write("i love you\n")
 
 # appending
-with open("my_file.txt",mode="a") as file:
+with open("my_file.txt", mode="a") as file:
     file.write("i love you")
 
 
